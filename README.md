349710d70878577b4602bff6508414731944544f
Bochra Arfaoui <bochraarfaoui@insat.u-carthage.tn>
2022-04-17 17:44:09 +0100

    Setup themes and fonts

---
+++ README.md
@@ -0,0 +1,4 @@
+# voting_app
+
+This is an e-voting app.
+
---
+++ lib/main.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.grey,
+        textTheme: const TextTheme(
+          headline1: TextStyle(
+              fontFamily: 'Poppins',
+              fontWeight: FontWeight.w600,
+              fontSize: 35,
+              color: Colors.white),
+          headline2: TextStyle(
+              fontFamily: 'Poppins',
+              fontWeight: FontWeight.w600,
+              fontSize: 28,
+              color: Colors.white),
+          subtitle1: TextStyle(
+              fontFamily: 'Poppins',
+              fontWeight: FontWeight.w500,
+              fontSize: 25,
+              color: Colors.white),
+          subtitle2: TextStyle(
+              fontFamily: 'Poppins',
+              fontWeight: FontWeight.w500,
+              fontSize: 20,
+              color: Colors.white),
+          bodyText1: TextStyle(
+              fontFamily: 'DM Sans',
+              fontWeight: FontWeight.w400,
+              fontSize: 17.5,
+              color: Colors.white),
+          bodyText2: TextStyle(
+              fontFamily: 'DM Sans',
+              fontWeight: FontWeight.w400,
+              fontSize: 15,
+              color: Colors.white),
+        ),
+        backgroundColor: const Color(0x00181717),
+        scaffoldBackgroundColor: const Color(0x00181717),
+        textButtonTheme: TextButtonThemeData(
+          style: TextButton.styleFrom(
+            alignment: AlignmentDirectional.center,
+            shape: const RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(16))),
+            textStyle: const TextStyle(
+                fontFamily: 'Poppins',
+                fontWeight: FontWeight.w500,
+                fontSize: 12,
+                color: Colors.white),
+          ),
+        ),
+        inputDecorationTheme: const InputDecorationTheme(
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(16)),
+            borderSide: BorderSide(color: Color(0x00FFB61D)),
+          ),
+          hintStyle: TextStyle(
+            fontFamily: 'DM Sans',
+            fontWeight: FontWeight.w400,
+            fontSize: 14,
+          ),
+          contentPadding: EdgeInsets.only(top: 16, left: 20),
+          filled: true,
+          fillColor: Colors.white,
+        ),
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
